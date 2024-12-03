43adc063f9cf279297ca44c86a6bfa99d9b4598c
Tomas Ian Choi <tic3@brg-zhang-xcel.ece.cornell.edu>
2024-12-03 14:42:37 -0500

    add interface pragma to 8 channel

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -156,25 +156,32 @@ extern "C" void bfs_xcel (
     int num_hops
 ) {
 
-  int pe_counter_buf[NUM_PE];
-  int pe_data0_buf[BFS_SIZE];
-  int pe_data1_buf[BFS_SIZE];
-  int pe_data2_buf[BFS_SIZE];
-  int pe_data3_buf[BFS_SIZE];
-  int pe_data4_buf[BFS_SIZE];
-  int pe_data5_buf[BFS_SIZE];
-  int pe_data6_buf[BFS_SIZE];
-  int pe_data7_buf[BFS_SIZE];
+  #pragma HLS INTERFACE m_axi port=pe_data0 offset=slave bundle=gmem0
+  #pragma HLS INTERFACE m_axi port=pe_data1 offset=slave bundle=gmem1
+  #pragma HLS INTERFACE m_axi port=pe_data2 offset=slave bundle=gmem2
+  #pragma HLS INTERFACE m_axi port=pe_data3 offset=slave bundle=gmem3
+  #pragma HLS INTERFACE m_axi port=pe_data4 offset=slave bundle=gmem4
+  #pragma HLS INTERFACE m_axi port=pe_data5 offset=slave bundle=gmem5
+  #pragma HLS INTERFACE m_axi port=pe_data6 offset=slave bundle=gmem6
+  #pragma HLS INTERFACE m_axi port=pe_data7 offset=slave bundle=gmem7
+  #pragma HLS INTERFACE m_axi port=pe_counter offset=slave bundle=gmem8
+  #pragma HLS INTERFACE m_axi port=last_frontier offset=slave bundle=gmem9
 
+  #pragma HLS INTERFACE s_axilite port=pe_data0 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_data1 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_data2 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_data3 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_data4 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_data5 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_data6 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_data7 bundle=control
+  #pragma HLS INTERFACE s_axilite port=pe_counter bundle=control
+  #pragma HLS INTERFACE s_axilite port=last_frontier bundle=control
+  #pragma HLS INTERFACE s_axilite port=num_hops bundle=control
+  #pragma HLS INTERFACE s_axilite port=return bundle=control
+
+  int pe_counter_buf[NUM_PE];
   copy_to_local_buffer(pe_counter, pe_counter_buf, NUM_PE);
-  copy_to_local_buffer(pe_data0, pe_data0_buf, pe_counter[0]);
-  copy_to_local_buffer(pe_data1, pe_data1_buf, pe_counter[1]);
-  copy_to_local_buffer(pe_data2, pe_data2_buf, pe_counter[2]);
-  copy_to_local_buffer(pe_data3, pe_data3_buf, pe_counter[3]);
-  copy_to_local_buffer(pe_data4, pe_data4_buf, pe_counter[4]);
-  copy_to_local_buffer(pe_data5, pe_data5_buf, pe_counter[5]);
-  copy_to_local_buffer(pe_data6, pe_data6_buf, pe_counter[6]);
-  copy_to_local_buffer(pe_data7, pe_data7_buf, pe_counter[7]);
 
   // set all the elements in visited to 1 
   // since we will use as mask to eliminate elements already visited
@@ -199,8 +206,8 @@ extern "C" void bfs_xcel (
   // do many iterations
   for (int i = 0; i < num_hops; i++){
 
-    spmv_xcel(pe_data0_buf, pe_data1_buf, pe_data2_buf, pe_data3_buf, pe_data4_buf, pe_data5_buf, pe_data6_buf,
-         pe_data7_buf, frontier, new_frontier, pe_counter_buf);
+    spmv_xcel(pe_data0, pe_data1, pe_data2, pe_data3, pe_data4, pe_data5, pe_data6,
+         pe_data7, frontier, new_frontier, pe_counter_buf);
 
     // mark visited nodes
     for (int j = 0; j < BFS_SIZE; j++){
