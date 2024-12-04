c6ffb720b03f4a26471e9c91453dbbb695a7f612
Tomas Ian Choi <tic3@brg-zhang-xcel.ece.cornell.edu>
2024-12-04 14:55:27 -0500

    fix baseline

--- bfs_baseline/bfs.hpp
+++ bfs_baseline/bfs.hpp
@@ -19,23 +19,15 @@ constexpr int NUM_PE = 8;
 constexpr int ROWS_PER_PE = BFS_SIZE / NUM_PE;
 constexpr int ENTRIES_PER_PE = BFS_SIZE * ROWS_PER_PE;
 
-extern "C" void bfs (
+void bfs (
   int coo[BFS_SIZE],
   bit last_frontier[BFS_SIZE],
   int num_hops
 );
 
 extern "C" void bfs_xcel (
-  int pe_data0[BFS_SIZE],
-  int pe_data2[BFS_SIZE],
-  int pe_data1[BFS_SIZE],
-  int pe_data3[BFS_SIZE],
-  int pe_data4[BFS_SIZE],
-  int pe_data5[BFS_SIZE],
-  int pe_data6[BFS_SIZE],
-  int pe_data7[BFS_SIZE],
-  int pe_counter[NUM_PE],
-  int last_frontier[BFS_SIZE], // what we return
+  int coo[BFS_SIZE],
+  int last_frontier[BFS_SIZE],
   int num_hops
 );
 
