0f04bdeb68a468d781db5af2739b41f90eadc6cf
gabizon103 <ehg54@cornell.edu>
2024-12-08 14:04:34 -0500

    pls work

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -39,7 +39,9 @@ void write_to_vecbuf(int vecbuf[NUM_PE][VEC_SIZE], int in_vec[VEC_SIZE]){
 
   WRITE_TO_VECBUF_OUTER: for (int i = 0; i < VEC_SIZE; i++) {
     #pragma HLS pipeline
+    #pragma HLS unroll factor=16
     WRITE_TO_VECBUF_INNER: for (int j = 0; j < NUM_PE; j++) {
+      #pragma HLS unroll factor=8
       vecbuf[j][i] = in_vec[i];
     }
   }
@@ -56,24 +58,29 @@ void write_to_streams(hls::stream<int> &pe_coords, int pe_data[BFS_SIZE], int pe
 
 }
 
+void local_to_global(int out_vec[VEC_SIZE], int resbuf[NUM_PE][ROWS_PER_PE], int pe_id) {
+  for (int j = 0; j < ROWS_PER_PE; j++) {
+    #pragma HLS unroll factor=64
+    int global_row = pe_id + j * NUM_PE;
+    out_vec[global_row] = resbuf[pe_id][j];
+  }
+}
+
 void write_out_vec(int out_vec[VEC_SIZE], int resbuf[NUM_PE][ROWS_PER_PE]){
+  #pragma HLS array_partition variable=out_vec type=block factor=64
+  #pragma HLS array_partition variable=resbuf dim=1 type=block factor=8
+  #pragma HLS array_partition variable=resbuf dim=2 type=block factor=64
 
   WRITE_OUT_VEC_ZERO_LOOP: for (int i = 0; i < VEC_SIZE; i++){
     #pragma HLS pipeline
+    #pragma HLS unroll factor=64
     out_vec[i] = 0;
   }
 
   WRITE_OUT_VEC_COPY_LOOP: for (int i = 0; i < NUM_PE; i++) {
-    #pragma HLS pipeline
-    int pe_id = i;
-    for (int j = 0; j < ROWS_PER_PE; j++) {
-      int global_row = pe_id + j * NUM_PE;
-      if (global_row < BFS_SIZE) { // just in case we set NUM_PE > BFS_SIZE (but i doubt it will happen)
-        out_vec[global_row] = resbuf[i][j];
-      }
-    }
+    #pragma HLS unroll factor=8
+    local_to_global(out_vec, resbuf, i);
   }
-
 }
 
 void spmv_xcel (
@@ -95,6 +102,7 @@ void spmv_xcel (
   // each PE has a local copy of the input vector
   int vecbuf[NUM_PE][VEC_SIZE];
   #pragma HLS array_partition variable=vecbuf dim=1
+  #pragma HLS array_partition variable=vecbuf dim=2 type=block factor=4
 
   // FIFOs for writing to each PE
   hls::stream<int> pe_coords0, pe_coords1, pe_coords2, pe_coords3,
@@ -114,6 +122,7 @@ void spmv_xcel (
 
   // write to each vecbuf
   write_to_vecbuf(vecbuf, in_vec);
+  #pragma HLS array_partition variable=in_vec type=block factor=16
 
   // write data in `coo` to `pe_coords`
   #pragma HLS array_partition variable=pe_counter
